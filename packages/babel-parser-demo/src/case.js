c0273c5ddc22f8851dc9b77ddd383685a6d1fd86
nbili <coding520@163.com>
2022-02-10 22:46:07 +0800

    init

--- packages/babel-parser-demo/src/case.js
+++ packages/babel-parser-demo/src/case.js
@@ -1,4 +1,5 @@
 const { parse } = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
 
 const code = `2 + ( 4 * 8)`;
 
@@ -6,4 +7,18 @@ const ast = parse(code);
 
 // console.log(JSON.stringify(ast, null, 2));
 
-console.log(JSON.stringify(ast.program.body[0], null, 2));
+// console.log(JSON.stringify(ast.program.body[0], null, 2));
+
+traverse(ast, {
+  NumericLiteral(path) {
+    console.log(path.node.value);
+  },
+  // NumericLiteral: {
+  //   enter(path) {
+  //     console.log(`Entered ${path.node.value}`);
+  //   },
+  //   exit(path) {
+  //     console.log(`Exit ${path.node.value}`);
+  //   },
+  // },
+});
