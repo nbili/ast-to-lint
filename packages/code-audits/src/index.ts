b25b3037a61120d44e32b1162f2779b02c7c0137
nbili <coding520@163.com>
2022-02-11 00:16:15 +0800

    code audits

--- packages/code-audits/src/index.ts
+++ packages/code-audits/src/index.ts
@@ -1,13 +1,37 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as glob from "glob";
+import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
 
 const files = glob.sync(
-  path.resolve(__dirname, "../../../examples/todo-list-react-hooks/src/**/*.js")
+  path.resolve(
+    __dirname,
+    "../../../examples/todo-list-react-hooks/src/**/*.jsx"
+  )
 );
 
 files.forEach((file) => {
   const contents = fs.readFileSync(file).toString();
 
-  console.log(contents);
+  const ast = parse(contents, { sourceType: "module", plugins: ["jsx"] });
+
+  traverse(ast, {
+    JSXOpeningElement({ node }) {
+      if (
+        node.name.type === "JSXIdentifier" &&
+        node.name.name === "IconButton"
+      ) {
+        node.attributes.forEach((attribute) => {
+          if (
+            attribute.type === "JSXAttribute" &&
+            attribute.name.type === "JSXIdentifier"
+          ) {
+            const propName = attribute.name.name;
+            console.log(propName);
+          }
+        });
+      }
+    },
+  });
 });
